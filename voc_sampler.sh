7d909e06bc08002b172e6fc61f01ea82cc4913b1
Alessandro Lekkas <alessandro.lekkas@startmail.com>
2024-09-28 20:16:32 +0100

    Generate multiple dataseeds in voc_sampler.sh, zip results

--- voc_sampler.sh
+++ voc_sampler.sh
@@ -4,15 +4,30 @@
 # PREV_DATASEED is the split number the novel classes files will be copied from, for a fair comparison
 # NEW_DATASEED is the split number where the base classes will be stored.
 
-if (( $# != 1 )); then
-    >&2 echo "Usage: bash voc_sampler.sh <voc_class_split=1, 2 or 3>"
+if (( $# != 2 )); then
+    >&2 echo "Usage: bash voc_sampler.sh <exp_name> <voc_class_split=1, 2 or 3>"
     exit
 fi
 
-VOC_CLASS_SPLIT=$1
+EXP_NAME=$1
+VOC_CLASS_SPLIT=$2
+
+SAMPLE_POOL_SIZE=100
+SAMPLE_SIZE=10
+ABLATION=False
 
 for ((i=0; i<10; i++)); do
   python BaseProtoSampler.py --num-gpus 1 --config-file configs/voc/defrcn_det_r101_base${VOC_CLASS_SPLIT}.yaml \
-  --opts OUTPUT_DIR sampler_logs/voc/seed$((30+i)) TEST.PCB_MODELPATH  "./ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth" \
-   SEED 0 PREV_DATASEED $i NEW_DATASEED $((30+i)) SAMPLE_SIZE 2 SAMPLE_POOL_SIZE 100
+  --opts OUTPUT_DIR sampler_logs/voc/${EXP_NAME}/seed$((30+i)) TEST.PCB_MODELPATH  "./ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth" \
+   SEED 0 PREV_DATASEED $i NEW_DATASEED $((30+i)) SAMPLE_SIZE $SAMPLE_SIZE SAMPLE_POOL_SIZE $SAMPLE_POOL_SIZE ABLATION $ABLATION
 done
+
+if [[ $ABLATION == True ]]; then
+  ZIP_NAME="voc_${EXP_NAME}_${SAMPLE_POOL_SIZE}_ablation.zip"
+else
+  ZIP_NAME="voc_${EXP_NAME}_${SAMPLE_POOL_SIZE}.zip"
+fi
+
+zip -r $ZIP_NAME datasets/vocsplit/seed3?
+mkdir -p voc_splits
+mv $ZIP_NAME voc_splits
