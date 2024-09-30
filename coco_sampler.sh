857fd8f701f7d85b365e084cf6e523fcae90d9df
Alessandro Lekkas <alessandro.lekkas@startmail.com>
2024-09-30 20:36:44 +0100

    Improve sampler zip naming

--- coco_sampler.sh
+++ coco_sampler.sh
@@ -10,21 +10,23 @@ EXP_NAME=$1
 SAMPLE_POOL_SIZE=100
 SAMPLE_OUT_SIZE="1 5 10"
 ABLATION=0
+RNGSEED=0
 
 for ((i=0; i<10; i++)); do
   python sampler.py --num-gpus 1 --config-file configs/coco/defrcn_det_r101_base.yaml \
   --sample_pool_size=$SAMPLE_POOL_SIZE --sample_out_size="$SAMPLE_OUT_SIZE" --ablation=$ABLATION \
   --prev_dataseed $i --new_dataseed  $((10+i)) \
   --opts OUTPUT_DIR sampler_logs/coco/seed$((10+i)) TEST.PCB_MODELPATH  "./ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth" \
-   SEED 0
+   SEED $RNGSEED
 done
 
-if [[ $ABLATION == True ]]; then
-  ZIP_NAME="coco_${EXP_NAME}_${SAMPLE_POOL_SIZE}_ablation.zip"
-else
-  ZIP_NAME="coco_${EXP_NAME}_${SAMPLE_POOL_SIZE}.zip"
-fi
+ZIP_NAME="coco_${EXP_NAME}_pool${SAMPLE_POOL_SIZE}"
 
-zip -r $ZIP_NAME datasets/cocosplit/seed1?
+if [[ $ABLATION == 1 ]]; then
+  ZIP_NAME+="_ablation"
+fi
+ZIP_NAME+="_rng${RNGSEED}"
+
+zip -r "$ZIP_NAME.zip" datasets/cocosplit/seed1?
 mkdir -p coco_splits
 mv $ZIP_NAME coco_splits
--- voc_sampler.sh
+++ voc_sampler.sh
@@ -11,21 +11,23 @@ VOC_CLASS_SPLIT=$2
 SAMPLE_POOL_SIZE=100
 SAMPLE_OUT_SIZE="1 5 10"
 ABLATION=0
+RNGSEED=0
 
 for ((i=0; i<10; i++)); do
   python sampler.py --num-gpus 1 --config-file configs/voc/defrcn_det_r101_base${VOC_CLASS_SPLIT}.yaml \
   --sample_pool_size=$SAMPLE_POOL_SIZE --sample_out_size="$SAMPLE_OUT_SIZE" --ablation=$ABLATION \
   --prev_dataseed $i --new_dataseed  $((30+i)) \
   --opts OUTPUT_DIR sampler_logs/voc/${EXP_NAME}/seed$((30+i)) TEST.PCB_MODELPATH  "./ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth" \
-   SEED 0
+   SEED $RNGSEED
 done
 
-if [[ $ABLATION == True ]]; then
-  ZIP_NAME="voc_${EXP_NAME}_${SAMPLE_POOL_SIZE}_ablation.zip"
-else
-  ZIP_NAME="voc_${EXP_NAME}_${SAMPLE_POOL_SIZE}.zip"
-fi
 
-zip -r $ZIP_NAME datasets/vocsplit/seed3?
+ZIP_NAME="voc${VOC_CLASS_SPLIT}_${EXP_NAME}_pool${SAMPLE_POOL_SIZE}"
+if [[ $ABLATION == 1 ]]; then
+  ZIP_NAME+="_ablation"
+fi
+ZIP_NAME+="_rng${RNGSEED}"
+
+zip -r "$ZIP_NAME.zip" datasets/vocsplit/seed3?
 mkdir -p voc_splits
 mv $ZIP_NAME voc_splits
